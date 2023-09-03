e2384230cd2f39710bd74826bd7e61f40948d3f5
Metaldetect <stas.klymenko@icloud.com>
2023-09-03 12:20:38 +0200

    Create contactSlice.js

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', phone: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', phone: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', phone: '227-91-26' },
+];
+
+export const contactSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts = [...state.contacts, action.payload];
+    },
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { deleteContact, addContact } = contactSlice.actions;
+
+export default contactSlice.reducer;
