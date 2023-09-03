2dec8bee84d8724e145b107db08d20f9661092fb
Metaldetect <stas.klymenko@icloud.com>
2023-09-03 12:24:31 +0200

    Create reducer.js

---
+++ src/redux/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import { contactSlice } from './contactSlice';
+import { filterSlice } from './filterSlice';
+
+export const reducer = combineReducers({
+  contactSlice,
+  filterSlice,
+});
