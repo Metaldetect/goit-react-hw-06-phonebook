1f3fa036fbbb16a8e36a3a88398dc2a5be2634bc
Metaldetect <stas.klymenko@icloud.com>
2023-09-03 12:28:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './styles/index.css';
 
+import { persistor, store } from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
