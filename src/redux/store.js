79fd5d1f0538e383581d1916567ed56feab69f2b
Metaldetect <stas.klymenko@icloud.com>
2023-09-04 17:06:28 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import rootReducer from './reducer';
 const persistConfig = {
   key: 'phonebook',
   storage,
-  // blacklist: ['filterSlice'],
+  blacklist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
