1b65acf43e2c24be503bf6c009741ad9a67bfc48
Metaldetect <stas.klymenko@icloud.com>
2023-09-05 03:54:02 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ContactForm from 'components/ContactForm';
 import ContactList from 'components/ContactList';
 import Filter from 'components/Filter';
@@ -8,17 +8,12 @@ import { getContacts } from 'redux/selectors';
 
 function App() {
   const contacts = useSelector(getContacts);
-  const [filter, setFilter] = useState('');
-
-  const handleFilterChange = value => {
-    setFilter(value);
-  };
 
   return (
     <>
       <ContactForm contacts={contacts} />
-      <Filter value={filter} onChange={handleFilterChange} />
-      <ContactList filter={filter} />
+      <Filter />
+      <ContactList />
     </>
   );
 }
